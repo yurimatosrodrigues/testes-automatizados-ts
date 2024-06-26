d250b78ab5fe58641065c268d7f0b2f5ec879fcc
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2024-06-25 22:10:11 -0300

    Adiciona testes mutantes com stryker

--- tests/model/NumeroConta.test.ts
+++ tests/model/NumeroConta.test.ts
@@ -1,21 +1,21 @@
 import { NumeroConta } from "../../src/model/NumeroConta";
 
 describe("Número Conta", () => {
-    test("número conta com seis dígitos", () => {
+    test("número conta com seis dígitos", async () => {
         const numeroConta: NumeroConta = new NumeroConta("123456");
         expect(numeroConta.numero).toBe("123456");
         expect(numeroConta.numero.length).toBe(6);
     });
 
-    test("número conta com cinco dígitos", () => {
+    test("número conta com cinco dígitos", async () => {
         expect(() => {new NumeroConta("12345")}).toThrow('número de conta inválido') ;
     });
 
-    test("número conta com sete dígitos", () => {
+    test("número conta com sete dígitos", async () => {
         expect(() => {new NumeroConta("1234567")}).toThrow('número de conta inválido') ;
     });
 
-    test("número conta com valor que não seja dígitos", () => {
+    test("número conta com valor que não seja dígitos", async () => {
         expect(() => {new NumeroConta("abc123")}).toThrow('número de conta inválido') ;
     })
 })
--- tests/model/Recibo.test.ts
+++ tests/model/Recibo.test.ts
@@ -5,4 +5,10 @@ describe("Recibo", () => {
         const recibo: Recibo = new Recibo();
         expect(recibo.codigo.length).toBe(6);
     });
+
+    test("validar código dos recibos", () => {
+        const recibo1: Recibo = new Recibo();
+        const recibo2: Recibo = new Recibo();
+        expect(recibo1.codigo).not.toBe(recibo2.codigo);
+    })
 });
