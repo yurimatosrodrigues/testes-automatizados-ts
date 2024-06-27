959f0279aa369e5223598cc3a4538a9c990d52dc
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2024-06-27 20:19:19 -0300

    adiciona cucumber e jenkins

--- tests/model/Recibo.test.ts
+++ tests/model/Recibo.test.ts
@@ -1,7 +1,7 @@
 import { Recibo } from "../../src/model/Recibo";
 
 describe("Recibo", () => {
-    test("criar com sucesso", async () => {
+    test("criar com sucesso", () => {
         const recibo: Recibo = new Recibo();
         expect(recibo.codigo.length).toBe(6);
     });
@@ -10,5 +10,12 @@ describe("Recibo", () => {
         const recibo1: Recibo = new Recibo();
         const recibo2: Recibo = new Recibo();
         expect(recibo1.codigo).not.toBe(recibo2.codigo);
-    })
+    });
+
+    test("criar recibos com código positivo", () => {
+        const recibo1: Recibo = new Recibo();
+        const recibo2: Recibo = new Recibo();
+        expect(BigInt(recibo1.codigo).valueOf()).toBeGreaterThan(0);
+        expect(BigInt(recibo2.codigo).valueOf()).toBeGreaterThan(0);
+    });
 });
